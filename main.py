56229e906f53325f52cbd85740e9022ad034176e
Harman0786 <5h26@solent.ac.uk>
2021-12-03 11:03:42 +0530

    UI Changes

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import tkinter as tk
+import tkinter.ttk as ttk
 from dialogs import CustomerDialog, AdvisorDialog
 
 class Main:
     def __init__(self):
         self.root = tk.Tk()
+        s=ttk.Style()
+        s.theme_use('classic')
 
         label1 = tk.Label(self.root, text="Solent Campers!")
         label1.pack(side=tk.TOP, pady=(20,0))
@@ -11,13 +14,13 @@ class Main:
         label1 = tk.Label(self.root, text="One Stop for All Campers")
         label1.pack(side=tk.TOP, pady=(20,0))
 
-        button1 = tk.Button(self.root, text="Customer", height=3, width=15, command=self.cutomerDialog)
+        button1 = tk.Button(self.root, text="Customer", height=3, width=15, bg='orange', command=self.cutomerDialog)
         button1.pack(side=tk.TOP, pady=(10, 10))
 
-        button2 = tk.Button(self.root, text="Advisor", height=3, width=15, command=self.advisorDialog)
+        button2 = tk.Button(self.root, text="Advisor", height=3, width=15, bg='green', command=self.advisorDialog)
         button2.pack(side=tk.TOP, pady=(10, 10))
 
-        button3 = tk.Button(self.root, text="Admin", height=3, width=15, command=self.adminDialog)
+        button3 = tk.Button(self.root, text="Admin", height=3, width=15, bg='red', command=self.adminDialog)
         button3.pack(side=tk.TOP, pady=(10, 10))
 
         self.root.title('Solent Campers - COM 714')
